51107f0d445d2c03e1a95a4aebdfe6a83c6e2eb4
lomoye <834033206@qq.com>
2018-08-06 11:08:13 +0800

    fix

--- src/main/java/com/xuxueli/crawler/model/RunConf.java
+++ src/main/java/com/xuxueli/crawler/model/RunConf.java
@@ -71,7 +71,7 @@ public class RunConf {
             return false;
         }
 
-        if (targetUrlRegex != null) {
+        if (targetUrlRegex != null && targetUrlRegex.length() > 0) {
             return RegexUtil.matches(targetUrlRegex, link);
         }
 
